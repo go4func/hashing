f55f62009e9aee51428299dc81f9c46eb6d7329c
Leo <nthlongtma@gmail.com>
2020-08-04 16:34:36 +0700

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module hashing
+
+go 1.14
+
+require (
+	github.com/pkg/errors v0.9.1
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	for {
+		// get password
+		raw, err := getInput("enter the password: ")
+		if err != nil {
+			panic(err)
+		}
+
+		// hash password
+		hashed, err := bCrypt(raw, bcrypt.DefaultCost)
+		if err != nil {
+			panic(err)
+		}
+		// encode version of hashed password
+		fmt.Printf("hashed password %s\n", hashed)
+
+		// get password again for comparing
+		raw, err = getInput("enter the password again: ")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("password match? %v\n\n", comparePasswords(hashed, raw))
+	}
+
+}
+
+func getInput(text string) ([]byte, error) {
+	fmt.Print(text)
+
+	password := make([]byte, 0)
+	_, err := fmt.Scan(&password)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to scan input")
+	}
+
+	return password, nil
+}
+
+func bCrypt(password []byte, cost int) ([]byte, error) {
+	start := time.Now()
+	defer func() {
+		fmt.Printf("bcrypt with cost= %d finished in %v(ms)\n", cost, time.Since(start).Milliseconds())
+	}()
+
+	hashed, err := bcrypt.GenerateFromPassword(password, cost)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to generate password")
+	}
+
+	return hashed, nil
+}
+
+func comparePasswords(hashed, raw []byte) bool {
+	if err := bcrypt.CompareHashAndPassword(hashed, raw); err != nil {
+		fmt.Println("passwords is not the same: ", err.Error())
+		return false
+	}
+	return true
+}
